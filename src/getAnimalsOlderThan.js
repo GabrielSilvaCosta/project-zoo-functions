c6e534ee050cd024118660c834d11ed9989743d8
GabrielSilvaCosta <bob@>
2023-03-09 17:15:52 -0300

    requisito 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animals = data.species.find((element) => element.name === animal);
+  if (!animals) {
+    return false;
+  }
+  return animals.residents.every((element) => element.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
