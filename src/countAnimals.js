201510a6131e5a610bcbe082f9d237f73dcb601d
GabrielSilvaCosta <bob@>
2023-03-10 11:27:32 -0300

    requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const valorInicial = {};
+
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, valorInicial);
+  }
+
+  const specie = data.species.find((element) => element.name === animal.species);
+  const sexFilter = animal.sex ? (resident) => resident.sex === animal.sex : () => true;
+  const sexResidents = specie.residents.filter(sexFilter);
+
+  return sexResidents.length;
 };
 
 module.exports = countAnimals;
