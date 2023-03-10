7c16a8be264f50b2c9d6f1cbed78c984e698d008
GabrielSilvaCosta <bob@>
2023-03-10 18:45:23 -0300

    requisito 10 atualizando...

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const cola = data.employees.find((element) => element.id === id);
+  const firstSpeciesId = cola.responsibleFor[0];
+  const animals = data.species.find((specie) => specie.id === firstSpeciesId).residents;
+  const sortedAnimals = animals.sort((a, b) => b.age - a.age);
+  const oldestAnimal = sortedAnimals[0];
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
